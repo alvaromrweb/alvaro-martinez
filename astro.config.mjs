8fdf0842bddb572226af307e4a679d6b0f9976b3
alvaromrweb <alvaromrweb@gmail.com>
2023-02-13 15:15:40 +0100

    Change domain settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://alvaromartinez.netlify.app/",
+  output: 'server',
   integrations: [
     tailwind(),
     image({
