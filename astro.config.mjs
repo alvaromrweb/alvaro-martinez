57c466ebeee92b6621bd06e78e8c71a99d48a322
alvaromrweb <alvaromrweb@gmail.com>
2023-02-15 14:31:48 +0100

    Add netlify adapter for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alvaromartinez.netlify.app/",
   output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind(),
     image({
